6731b764324bec9fa4acdbba93e7292a9287fe44
Sachin737 <mahawarsachin31@gmail.com>
2023-02-03 16:27:07 +0530

    learned Promise

--- async-js/index.js
+++ async-js/index.js
@@ -1,14 +1,44 @@
-const http = require('http');
+// Lets learn to solve Callback Hell using promises
+
+// Remember water boiling in kitchen with timer(callbacks)
+// VS
+// Ordering Burger getting reciept (promises)
+// ref: Coding with Chaim
+
+const { rejects } = require('assert');
 const fs = require('fs');
-const superagent = require('./node_modules/superagent');
 
-fs.readFile(`${__dirname}/dog.txt`, 'utf8', (err, data) => {
-  console.log(data);
-  superagent.get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .end((err, res) => {
-        if(err)console.log(err);
-        console.log(res.body);
+const readFilePro = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, 'utf-8', (err, data) => {
+      if (err) reject(`Coud not read file! ${file}`);
+      resolve(data);
     });
-});
+  });
+};
 
-// const server = http.createServer();
+const writeFilePro = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      if (err) reject('Coud not write to file!');
+      resolve('success!');
+    });
+  });
+};
+
+readFilePro('dog.txt')
+  .then((data) => {
+    console.log('Breed: ', data);
+    let data1 = `${data}007`;
+    return writeFilePro('dog1.txt', data1);
+  })
+  .then((res) => {
+    console.log('dog1.txt get created!');
+    return readFilePro('dog1.txt');
+  })
+  .then((ans) => {
+    console.log(ans);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
